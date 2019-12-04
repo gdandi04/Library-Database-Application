4053dd0085bf98cfefc11533e0758ede4dffa923
gdandi04 <gdandi04@gmail.com>
2019-12-04 14:20:01 -0500

    fixed search bugs

--- LibraryDatabase/src/libraryDB/InitializeConnection.java
+++ LibraryDatabase/src/libraryDB/InitializeConnection.java
@@ -1,55 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
-
-db.mysql.url="jdbc:mysql://localhost:3306/db?characterEncoding=UTF-8&useSSL=false"
- */
-
 package libraryDB;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.Scanner;
 
 /**
- *
  * @author kath
  */
 public class InitializeConnection {
 
-  /** The name of the MySQL account to use (or empty for anonymous) */
+  /**
+   * The name of the MySQL account to use (or empty for anonymous)
+   */
   private final String userName;
 
-  /** The password for the MySQL account (or empty for anonymous) */
+  /**
+   * The password for the MySQL account (or empty for anonymous)
+   */
   private final String password;
 
-  /** The name of the computer running MySQL */
+  /**
+   * The name of the computer running MySQL
+   */
   private final String serverName = "localhost";
 
-  /** The port of the MySQL server (default is 3306) */
+  /**
+   * The port of the MySQL server (default is 3306)
+   */
   private final int portNumber = 3306;
 
-  /** The name of the database we are testing with (this default is installed with MySQL) */
+  /**
+   * The name of the database we are testing with (this default is installed with MySQL)
+   */
   private final String dbName = "universityLibrarydb";
 
   private int userID;
 
   public enum Commands {
-    DELETE, HOLD, CHECKOUT, SEARCH, SIGNOUT, VIEW_HOLDS, VIEW_RENTED;
+    DELETE, HOLD, CHECKOUT, SEARCH,
+    VIEW_HOLDS, VIEW_RENTED, SIGNOUT;
   }
 
-  /** The name of the table we are testing with */
-//	private final String tableName = "JDBC_TEST";
+  /**
+   * The name of the table we are testing with
+   */
   private final boolean useSSL = false;
 
   public InitializeConnection() {
@@ -100,7 +97,6 @@ public class InitializeConnection {
     Connection conn = null;
     try {
       conn = this.getConnection();
-      System.out.println("Connected to database\n\n\n");
       this.runCommand(conn);
     } catch (SQLException e) {
       System.out.println("ERROR: Could not connect to the database");
@@ -109,92 +105,160 @@ public class InitializeConnection {
     }
   }
 
-    public void runCommand (Connection conn) {
-      this.possibleCommands();
+  public void runCommand(Connection conn) {
+    this.possibleCommands();
 
-      Scanner scan = new Scanner(System.in);
-      String command = scan.next();
+    Scanner scan = new Scanner(System.in);
+    String command = scan.next();
 
-      switch (command) {
-        case "DELETE":
-          try {
-            DeleteHold dh = new DeleteHold(this);
-            dh.deleteItem();
-          } catch (SQLException e) {
-            System.out.println("Unable to execute command.");
-            e.printStackTrace();
-          }
-          break;
+    switch (command.toUpperCase()) {
+      case "DELETE":
+        try {
+          DeleteHold dh = new DeleteHold(this);
+          dh.deleteItem();
+        } catch (SQLException e) {
+          System.out.println("Unable to execute command.");
+          e.printStackTrace();
+        }
+        break;
 
-        case "HOLD":
-          try {
-            PlaceHold ph = new PlaceHold(this, conn);
-            ph.placeItemOnHold();
-          } catch (SQLException e) {
-            System.out.println("Unable to execute command");
-            e.printStackTrace();
-          }
-          break;
-
-        case "CHECKOUT":
-          try {
-            CheckOut ch = new CheckOut(this, conn);
-            ch.showAllMedia();
-            ch.rentMedia();
-
-          } catch (SQLException e) {
-            e.printStackTrace();
-
-          }
-          break;
-
-        case "SEARCH":
-          SearchMedia search = new SearchMedia(this);
-          try {
-            search.searchResults();
-          } catch (SQLException e) {
-            System.out.println("Unable to execute command.");
-            e.printStackTrace();
-          }
-          break;
-
-        case "VIEW_HOLDS":
+      case "HOLD":
+        try {
           PlaceHold ph = new PlaceHold(this, conn);
-          System.out.println(ph.printBooksOnHold() + "\n\n\n");
-          this.runCommand(conn);
-          break;
+          ph.placeItemOnHold();
+        } catch (SQLException e) {
+          System.out.println("Unable to execute command");
+          e.printStackTrace();
+        }
+        break;
 
-        case "VIEW_RENTED":
+      case "CHECKOUT":
+        try {
           CheckOut ch = new CheckOut(this, conn);
-          System.out.println(ch.printRentedMedia() + "\n\n\n");
-          this.runCommand(conn);
-          break;
+          ch.showAllMedia();
+          ch.rentMedia();
 
-        case "SIGNOUT":
-          System.out.println("Would you like to sign out?\n\'Yes\' or \'No\'");
-          String s = scan.next();
-            if (s.equals("Yes")) {
-              System.out.println("Goodbye");
-              scan.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+
+        }
+        break;
+
+      case "SEARCH":
+        SearchMedia sm = new SearchMedia(this);
+        SearchMedia searchMedia = new SearchMedia(this);
+        try {
+          String mediaType = this.userChoice();
+
+          String mediaQuery = this.searchQuery();
+          switch (mediaType) {
+            case "1":
+              System.out.println("Searching Books");
+              searchMedia.searchBook(mediaQuery);
+              this.exitOut(mediaType);
+              mediaQuery = this.searchQuery();
               break;
-            } else if (s.equals("No")) {
-              this.runCommand(conn);
-            }
-            break;
-        default:
-          System.out.println("Invalid command\n");
+            case "2":
+              System.out.println("Searching E-Books");
+              searchMedia.searchEBook(mediaQuery);
+              this.exitOut(mediaType);
+              mediaQuery = this.searchQuery();
+              break;
+            case "3":
+              System.out.println("Searching Videos");
+              searchMedia.searchVideo(mediaQuery);
+              this.exitOut(mediaType);
+              mediaQuery = this.searchQuery();
+              break;
+            case "4":
+              System.out.println("Searching CDs");
+              searchMedia.searchCD(mediaQuery);
+              this.exitOut(mediaType);
+              mediaQuery = this.searchQuery();
+              break;
+            case "exit":
+              this.run();
+              break;
+            default:
+              System.out.println("The library doesn't carry this media type");
+          }
+        } catch (SQLException e) {
+          System.out.println("Unable to execute command.");
+          e.printStackTrace();
+        }
+        break;
+
+      case "VIEW_HOLDS":
+        PlaceHold ph = new PlaceHold(this, conn);
+        System.out.println(ph.printBooksOnHold() + "\n\n\n");
+        this.runCommand(conn);
+        break;
+
+      case "VIEW_RENTED":
+        CheckOut ch = new CheckOut(this, conn);
+        System.out.println(ch.printRentedMedia() + "\n\n\n");
+        this.runCommand(conn);
+        break;
+
+      case "SIGNOUT":
+        System.out.println("Would you like to sign out?\n\'Yes\' or \'No\'");
+        String s = scan.next();
+        if (s.equalsIgnoreCase("Yes")) {
+          System.out.println("Goodbye");
+          scan.close();
+          break;
+        } else if (s.equalsIgnoreCase("No")) {
           this.runCommand(conn);
-      }
-      scan.close();
+        }
+        break;
+      default:
+        System.out.println("Invalid command\n");
+        this.runCommand(conn);
     }
+  }
 
   int getUserID() {
     return this.userID;
   }
 
+  private boolean checkIfExit(String choice) {
+    if (choice.equalsIgnoreCase("exit")) {
+      return true;
+    }
+    return false;
+  }
+
   private void possibleCommands() {
     System.out.println("Please enter one of the following possible commands: ");
     System.out.println(Arrays.asList(Commands.values()));
+  }
+
+  private void exitOut(String mediaType) throws SQLException {
+    mediaType = this.userChoice();
+    if (this.checkIfExit(mediaType)) {
+      this.runCommand(this.getConnection());
+    }
+  }
+
+  private void displayMediaTypes() {
+    System.out.println(
+        "Choose the type of media you would like to search for by typing the corresponding number.");
+    System.out.println("1: Book\n"
+        + "2: E-Book\n"
+        + "3: Video\n"
+        + "4: CD");
+  }
+
+  private String userChoice() {
+    Scanner choice = new Scanner(System.in);
+    this.displayMediaTypes();
+    return choice.next();
+  }
+
+  private String searchQuery() {
+    Scanner media = new Scanner(System.in);
+    System.out.println("Enter your search query:");
+    return media.next();
   }
 
 }
--- LibraryDatabase/src/libraryDB/SearchMedia.java
+++ LibraryDatabase/src/libraryDB/SearchMedia.java
@@ -1,6 +1,9 @@
 package libraryDB;
 
+
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,11 +13,10 @@ import java.util.Scanner;
 public class SearchMedia {
 
   private InitializeConnection db;
-  private String book;
   private Connection conn;
 
-  public SearchMedia(InitializeConnection db) {
-    this.db = db;
+  public SearchMedia(InitializeConnection init) {
+    this.db = init;
     try {
       this.conn = db.getConnection();
     } catch (SQLException e) {
@@ -23,44 +25,134 @@ public class SearchMedia {
     }
   }
 
-  public void searchResults() throws SQLException {
-    ArrayList<ArrayList<String>> media = new ArrayList<>();
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Enter a search query:");
-   // while (scan.hasNext()) {
-    String next = scan.nextLine();
-    Scanner nextItem = new Scanner(next);
-    while (nextItem.hasNext()) {
-      String query = nextItem.next();
-      System.out.println(query);
-      if (next.equals("exitSearch")) {
-        System.out.println("Exited search command, back to homepage");
-      } else {
-        if (this.searchHelp(query)) {
-          Statement search = conn.createStatement();
-          ResultSet searchResult =
-                search.executeQuery("SELECT * FROM media WHERE media_title LIKE '%"
-                      + next + "%'");
-          while (searchResult.next()) {
-            ArrayList<String> row = new ArrayList<String>();
-            row.add("Media ID: " + searchResult.getString("media_id"));
-            row.add("Media Name: " + searchResult.getString("media_title"));
-            media.add(row);
-            System.out.println(row);
-          }
+  public void searchBook(String item) throws SQLException {
+    CallableStatement search = db.getConnection().prepareCall("{CALL search_book(?)}");
+    search.setString("search_query", item);
+    ResultSet rs = search.executeQuery();
+
+    ArrayList<ArrayList<String>> results = new ArrayList<>();
+
+    while (rs.next()) {
+      ArrayList<String> row = new ArrayList<>();
+      row.add("Book ID: " + rs.getString("book_id"));
+      row.add("Title: " + rs.getString("title"));
+      row.add("Author: " + rs.getString("author"));
+      row.add("Genre: " + rs.getString("genre"));
+      row.add("Plot: " + rs.getString("plot"));
+      row.add("Year of Publication: " + rs.getString("pubYear"));
+      row.add("Page Count: " + rs.getString("pageCount"));
+      row.add("Number of Copies Available: " + rs.getString("numCopies"));
+      results.add(row);
+    }
+
+    if (results.isEmpty()) {
+      System.out.println("No items found that meet this criteria");
+    } else {
+      for (ArrayList<String> list : results) {
+        for (String s : list) {
+          System.out.println(s);
         }
       }
     }
-//    for (ArrayList<String> list : media) {
-//      for (String s : list) {
-//        System.out.println(s);
-//      }
-//    }
+  }
+
+  public void searchEBook(String item) throws SQLException {
+    CallableStatement search = db.getConnection().prepareCall("{CALL search_ebook(?)}");
+    search.setString("search_query", item);
+    ResultSet rs = search.executeQuery();
+
+    ArrayList<ArrayList<String>> results = new ArrayList<>();
+
+    while (rs.next()) {
+      ArrayList<String> row = new ArrayList<>();
+      row.add("Book ID: " + rs.getString("book_id"));
+      row.add("Title: " + rs.getString("title"));
+      row.add("Author: " + rs.getString("author"));
+      row.add("Genre: " + rs.getString("genre"));
+      row.add("Plot: " + rs.getString("plot"));
+      row.add("Year of Publication (print): " + rs.getString("printPubYear"));
+      row.add("Year of Publication (online): " + rs.getString("ePubYear"));
+      row.add("Page Count: " + rs.getString("pageCount"));
+      row.add("Number of Copies Available: " + rs.getString("num_copies"));
+      results.add(row);
+    }
+
+    if (results.isEmpty()) {
+      System.out.println("No items found that meet this criteria");
+    } else {
+      for (ArrayList<String> list : results) {
+        for (String s : list) {
+          System.out.println(s);
+        }
+      }
+    }
+  }
+
+  public void searchVideo(String item) throws SQLException {
+    CallableStatement search = db.getConnection().prepareCall("{CALL search_video(?)}");
+    search.setString("search_query", item);
+    ResultSet rs = search.executeQuery();
+
+    ArrayList<ArrayList<String>> results = new ArrayList<>();
+
+    while (rs.next()) {
+      ArrayList<String> row = new ArrayList<>();
+      row.add("Video ID: " + rs.getString("video_id"));
+      row.add("Title: " + rs.getString("title"));
+      row.add("Director: " + rs.getString("director"));
+      row.add("Actors: " + rs.getString("actors"));
+      row.add("Genre: " + rs.getString("genre"));
+      row.add("Plot: " + rs.getString("plot"));
+      row.add("Runtime: " + rs.getString("runtime"));
+      row.add("Year of Release: " + rs.getString("year_released"));
+      row.add("Number of Copies Available: " + rs.getString("num_copies"));
+      results.add(row);
+    }
+
+    if (results.isEmpty()) {
+      System.out.println("No items found that meet this criteria");
+    } else {
+      for (ArrayList<String> list : results) {
+        for (String s : list) {
+          System.out.println(s);
+        }
+      }
+    }
+  }
+
+  public void searchCD(String item) throws SQLException {
+    CallableStatement search = db.getConnection().prepareCall("{CALL search_cd(?)}");
+    search.setString("search_query", item);
+    ResultSet rs = search.executeQuery();
+
+    ArrayList<ArrayList<String>> results = new ArrayList<>();
+
+    while (rs.next()) {
+      ArrayList<String> row = new ArrayList<>();
+      row.add("CD ID: " + rs.getString("cd_id"));
+      row.add("Album Name: " + rs.getString("album_name"));
+      row.add("Artist: " + rs.getString("artist"));
+      row.add("Genre: " + rs.getString("genre"));
+      row.add("Song List: " + rs.getString("song_list"));
+      row.add("Year of Release: " + rs.getString("year_released"));
+      row.add("Number of Copies Available: " + rs.getString("num_copies"));
+      results.add(row);
+    }
+
+    if (results.isEmpty()) {
+      System.out.println("No items found that meet this criteria");
+    } else {
+      for (ArrayList<String> list : results) {
+        for (String s : list) {
+          System.out.println(s);
+        }
+      }
+    }
   }
 
   private boolean searchHelp(String oneWord) throws SQLException {
     Statement search = conn.createStatement();
-    return search.execute("SELECT * FROM media WHERE media_title LIKE '%" + oneWord + "%'");
+    return search.execute("SELECT * FROM media WHERE media_name LIKE %" + oneWord + "%");
   }
 
 }
